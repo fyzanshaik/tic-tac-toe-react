e2eab6346b784de9df008c67b817da870708cb89
fyzanshaik <fyzan.shaik@gmail.com>
2024-10-10 16:47:13 +0530

    Code clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useCallback, useState } from "react";
-import PlayerInfo from "./components/Player-info"
-import GameBoard from "./components/GameBoard"
+import { useCallback, useState, useMemo } from "react";
+import PlayerInfo from "./components/Player-info";
+import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import GameOver from "./components/GameOver";
 import WINNING_COMBINATIONS from "./winning-combinations";
@@ -12,87 +12,82 @@ const initialGameBoard = [
 ];
 
 const derivedActivePlayer = (gameTurns) => {
-  let currentPlayer = 'X';
-  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
-    currentPlayer = 'O';
-  }
-  return currentPlayer;
-}
-
-const derivedWinner = (gameBoard, playerData) => {
-
-
-  return winner;
-}
+  return gameTurns.length % 2 === 0 ? 'X' : 'O';
+};
 
 const App = () => {
-  // const [activePlayer, setActivePlayer] = useState('X');
-  const [gameTurns, setGameTurns] = useState([]); //gameTurns : [{square:{row:1,col:0},player:X||O}]
+  const [gameTurns, setGameTurns] = useState([]);
   const [playerData, setPlayerData] = useState({
     X: "Player 1",
     O: "Player 2"
-  })
+  });
 
   const handlePlayerDataChange = useCallback((symbol, newName) => {
-    setPlayerData((prevPlayerData) => {
-      return {
-        ...prevPlayerData,
-        [symbol]: newName
+    setPlayerData((prevPlayerData) => ({
+      ...prevPlayerData,
+      [symbol]: newName
+    }));
+  }, []);
+
+  const gameBoard = useMemo(() => {
+    const board = initialGameBoard.map(arr => [...arr]);
+    gameTurns.forEach(({ square, player }) => {
+      const { row, col } = square;
+      board[row][col] = player;
+    });
+    return board;
+  }, [gameTurns]);
+
+  const winner = useMemo(() => {
+    for (const combination of WINNING_COMBINATIONS) {
+      const [a, b, c] = combination;
+      const firstSymbol = gameBoard[a.row][a.column];
+      if (firstSymbol && firstSymbol === gameBoard[b.row][b.column] && firstSymbol === gameBoard[c.row][c.column]) {
+        return playerData[firstSymbol];
       }
-    })
-  }, [])
-  let gameBoard = [...initialGameBoard.map(arr => [...arr])];
-
-  let winner = null
-  const activePlayer = derivedActivePlayer(gameTurns);
-
-  for (const eachTurn of gameTurns) {
-    const { square, player } = eachTurn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
-  for (const combinations of WINNING_COMBINATIONS) {
-    const firstSymbol = gameBoard[combinations[0].row][combinations[0].column];
-    const secondSymbol = gameBoard[combinations[1].row][combinations[1].column];
-    const thirdSymbol = gameBoard[combinations[2].row][combinations[2].column];
-    if (firstSymbol && firstSymbol == secondSymbol && secondSymbol == thirdSymbol) {
-      winner = playerData[firstSymbol];
     }
+    return null;
+  }, [gameBoard, playerData]);
 
-  }
+  const isDraw = useMemo(() => gameTurns.length === 9 && !winner, [gameTurns, winner]);
 
-  let drawCheck = gameTurns.length === 9 && !winner;
-  const handleRestart = () => {
+  const handleRestart = useCallback(() => {
     setGameTurns([]);
-    winner = null; drawCheck = null;
-
-  }
+  }, []);
 
   const handleChangeActivePlayer = useCallback((rowIndex, colIndex) => {
-    // setActivePlayer((prevActivePlayer) => prevActivePlayer === 'X' ? 'O' : 'X')
-    setGameTurns(prevTurns => {
+    setGameTurns((prevTurns) => {
       const currentPlayer = derivedActivePlayer(prevTurns);
-      const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns]
-
+      const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns];
       return updatedTurns;
-    })
-  }, [])
+    });
+  }, []);
+
+  const activePlayer = useMemo(() => derivedActivePlayer(gameTurns), [gameTurns]);
 
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <PlayerInfo initialName="Player 1" symbolName="X" isActive={activePlayer === 'X'} onChangeName={handlePlayerDataChange} ></PlayerInfo>
-          <PlayerInfo initialName="Player 2" symbolName="O" isActive={activePlayer === 'O'} onChangeName={handlePlayerDataChange}></PlayerInfo>
+          <PlayerInfo
+            initialName="Player 1"
+            symbolName="X"
+            isActive={activePlayer === 'X'}
+            onChangeName={handlePlayerDataChange}
+          />
+          <PlayerInfo
+            initialName="Player 2"
+            symbolName="O"
+            isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerDataChange}
+          />
         </ol>
-        {(winner || drawCheck) && <GameOver winnerSymbol={winner} restart={handleRestart} />}
-        <GameBoard onChangeActivePlayer={handleChangeActivePlayer} board={gameBoard} ></GameBoard>
+        {(winner || isDraw) && <GameOver winnerSymbol={winner} restart={handleRestart} />}
+        <GameBoard onChangeActivePlayer={handleChangeActivePlayer} board={gameBoard} />
       </div>
-      <Log turns={gameTurns} playerData={playerData}></Log>
+      <Log turns={gameTurns} playerData={playerData} />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
