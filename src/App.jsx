901687b4a9ecb094c6ab209baaefe785222a2e28
fyzanshaik <fyzan.shaik@gmail.com>
2024-10-10 15:25:49 +0530

    Game winning functionality implemented

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const derivedActivePlayer = (gameTurns) => {
 const App = () => {
   // const [activePlayer, setActivePlayer] = useState('X');
   const [gameTurns, setGameTurns] = useState([]); //gameTurns : [{square:{row:1,col:0},player:X||O}]
-
+  let winner = null;
   const activePlayer = derivedActivePlayer(gameTurns);
 
   let gameBoard = initialGameBoard;
@@ -32,6 +32,17 @@ const App = () => {
     const { row, col } = square;
 
     gameBoard[row][col] = player;
+  }
+
+  for (const combinations of WINNING_COMBINATIONS) {
+    const firstSymbol = gameBoard[combinations[0].row][combinations[0].column];
+    const secondSymbol = gameBoard[combinations[1].row][combinations[1].column];
+    const thirdSymbol = gameBoard[combinations[2].row][combinations[2].column];
+
+    if (firstSymbol && firstSymbol == secondSymbol && secondSymbol == thirdSymbol) {
+      winner = firstSymbol;
+    }
+
   }
 
   const handleChangeActivePlayer = useCallback((rowIndex, colIndex) => {
@@ -51,6 +62,7 @@ const App = () => {
           <PlayerInfo initialName="Player 1" symbolName="X" isActive={activePlayer === 'X'} ></PlayerInfo>
           <PlayerInfo initialName="Player 2" symbolName="O" isActive={activePlayer === 'O'}></PlayerInfo>
         </ol>
+        {winner && <p>You won! {winner}!</p>}
         <GameBoard onChangeActivePlayer={handleChangeActivePlayer} board={gameBoard}></GameBoard>
       </div>
       <Log turns={gameTurns}></Log>
